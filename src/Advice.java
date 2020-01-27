75fa2e3e29903ce4cefd04471e6fc0d52256dbda
91068412 <isafra3.02@gmail.com>
2020-01-27 13:55:13 -0800

    Copied from textbook Done

---
+++ src/Advice.java
@@ -0,0 +1,11 @@
+public class Advice extends Thought
+{
+    public void message()
+    {
+        System.out.println ("Think before you speak.");
+        super.message();
+    }
+}
+/* Isabela Francisco
+p.5
+ */
---
+++ src/Thought.java
@@ -0,0 +1,11 @@
+public class Thought
+{
+    public void message()
+    {
+        System.out.println ("I'm thinking, I'm thinking.");
+        System.out.println ();
+    }
+}
+/* Isabela Francisco
+p.5
+ */
